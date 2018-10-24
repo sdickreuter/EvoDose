a0dcedac500b4bdf6b6ea1abe9c9f5306606beed
Aypac <github@aypac.de>
2018-10-24 12:32:46 +0000

    Adapt new matplotlib conventions, increase contrast in dose map

--- plot_functions.py
+++ plot_functions.py
@@ -28,11 +28,12 @@ def calc_dose_map(x0, y0, doses, stepsize=1):
 
 def plot_dose_map(filename, x, y, exposure):
     plot = plt.imshow(np.flipud(exposure), extent=[np.min(x), np.max(x), np.min(y), np.max(y)])
+    axis = plot.axes
     cb = plt.colorbar()
     cb.set_label(r'$Dose\, / \, \frac{\mu C}{cm^2} ')
-    plt.contour(x, y, exposure, [parameters.target_dose])
-    plt.xlabel(r'$x\,  /\,  nm')
-    plt.ylabel(r'$y\,  /\,  nm')
+    axis.contour(x, y, exposure, [parameters.target_dose], colors=['yellow'])
+    axis.set_xlabel(r'$x\,  /\,  nm')
+    axis.set_ylabel(r'$y\,  /\,  nm')
     plt.tight_layout(.5)
     plt.savefig(filename, dpi=600)
     plt.close()
@@ -41,11 +42,12 @@ def plot_dose_map(filename, x, y, exposure):
 def plot_expected_shape(filename, x, y, exposure, x0, y0, cx, cy):
     plot = plt.imshow(np.flipud(exposure >= parameters.target_dose),
                       extent=[np.min(x), np.max(x), np.min(y), np.max(y)])
-    plt.scatter(x0, y0, c="blue", s=0.4)
-    plt.scatter(cx.ravel(), cy.ravel(), c="red", s=0.4)
-    plt.axes().set_aspect('equal')
-    plt.xlabel(r'$x\,  /\,  nm')
-    plt.ylabel(r'$y\,  /\,  nm')
+    axis = plot.axes
+    axis.scatter(x0, y0, c="blue", s=0.4)
+    axis.scatter(cx.ravel(), cy.ravel(), c="red", s=0.4)
+    axis.set_aspect('equal')
+    axis.set_xlabel(r'$x\,  /\,  nm')
+    axis.set_ylabel(r'$y\,  /\,  nm')
     plt.tight_layout()
     plt.savefig(filename, dpi=600)
     plt.close()
@@ -63,10 +65,11 @@ def plot_convergence(filename, t, convergence):
 def plot_exposurepoints(filename, x0, y0, doses, scalefactor=15):
     area = np.pi * (scalefactor * doses / np.max(doses)) ** 2
     # area = np.pi * ( 0.005*(np.max(y)-np.min(y)) * repetitions / np.max(repetitions)) ** 2
-    plt.scatter(x0, y0, s=area, alpha=0.5, edgecolors="black", linewidths=1)
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.xlabel(r'$x\,  /\,  nm')
-    plt.ylabel(r'$y\,  /\,  nm')
+    plot = plt.scatter(x0, y0, s=area, alpha=0.5, edgecolors="black", linewidths=1)
+    axis = plot.axes
+    axis.set_aspect('equal', 'datalim')
+    axis.set_xlabel(r'$x\,  /\,  nm')
+    axis.set_ylabel(r'$y\,  /\,  nm')
     plt.tight_layout()
     plt.savefig(filename, dpi=600)
     plt.close()
@@ -75,11 +78,12 @@ def plot_exposurepoints(filename, x0, y0, doses, scalefactor=15):
 def plot_setup(filename: str, struct: Structure, scalefactor: float = 1):
     assert scalefactor > 0
     area = 0.4 * scalefactor
-    plt.scatter(struct[0], struct[1], s=area, alpha=0.5, c='red', edgecolors="black", linewidths=1)
-    plt.scatter(struct[2], struct[3], s=area, alpha=0.5, c='blue', edgecolors="black", linewidths=1)
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.xlabel(r'$x\,  /\,  nm')
-    plt.ylabel(r'$y\,  /\,  nm')
+    plot = plt.scatter(struct[0], struct[1], s=area, alpha=0.5, c='red')
+    axis = plot.axes
+    axis.scatter(struct[2], struct[3], s=area, alpha=0.5, c='blue')
+    axis.set_aspect('equal', 'datalim')
+    axis.set_xlabel(r'$x\,  /\,  nm')
+    axis.set_ylabel(r'$y\,  /\,  nm')
     plt.tight_layout()
     plt.savefig(filename, dpi=600)
     plt.close()
