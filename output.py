1c5095ff9140c4d74345f04a3bae9267c183c308
Aypac <github@aypac.de>
2018-10-26 10:00:11 +0000

    Basic Code cleanup + Use basic PIP auto-formatting + Adding PEP 484 type hints + Remove unclear print statement

--- output.py
+++ output.py
@@ -1,24 +1,25 @@
 import parameters
 import numpy as np
 
-def output_xenos(outfilename,patternname,x0,y0,doses):
-    repetitions = doses / (parameters.dwell_time*parameters.current)
+
+def output_xenos(outfilename: str, patternname: str, x0, y0, doses) -> None:
+    repetitions = doses / (parameters.dwell_time * parameters.current)
     repetitions[np.where(repetitions < 1)] = 0
     repetitions = np.array(np.round(repetitions), dtype=np.int)
 
-    Outputfile = open(outfilename,'w')
+    Outputfile = open(outfilename, 'w')
 
-    #Outputfile.write('D line_test, 11000, 11000, 5, 5' + '\n')
-    Outputfile.write('D '+ patternname + '\n')
+    # Outputfile.write('D line_test, 11000, 11000, 5, 5' + '\n')
+    Outputfile.write('D ' + patternname + '\n')
     Outputfile.write('I 1' + '\n')
-    Outputfile.write('C '+str(int(parameters.dwell_time*1e9)) + '\n')
+    Outputfile.write('C ' + str(int(parameters.dwell_time * 1e9)) + '\n')
     Outputfile.write("FSIZE 15 micrometer" + '\n')
     Outputfile.write("UNIT 1 micrometer" + '\n')
 
-    print(repetitions)
+    # print(repetitions)
     for j in range(len(x0)):
         if repetitions[j] >= 1:
-            Outputfile.write('RDOT '+str(x0[j]) + ', ' + str(y0[j]) + ', ' + str((repetitions[j])) + '\n')
+            Outputfile.write('RDOT ' + str(x0[j]) + ', ' + str(y0[j]) + ', ' + str((repetitions[j])) + '\n')
     Outputfile.write('END' + '\n')
     Outputfile.write('\n')
     Outputfile.write('\n')
@@ -26,11 +27,11 @@ def output_xenos(outfilename,patternname,x0,y0,doses):
     Outputfile.close()
 
 
-def output_raith(outfilename,layer,x0,y0,doses):
+def output_raith(outfilename:str, layer: int, x0, y0, doses) -> None:
     rel_doses = np.round(doses / doses.max() * 100)
     with open(outfilename, 'w') as Outputfile:
         for i in range(len(rel_doses)):
             Outputfile.write('P ' + str(int(rel_doses[i])) + ' ' + str(layer) + ' 0 ' + '\n')
             Outputfile.write(
-                        str(round(x0[i], 3)) + ' ' + str(round(y0[i], 3)) + '\n')
+                str(round(x0[i], 3)) + ' ' + str(round(y0[i], 3)) + '\n')
             Outputfile.write('#\n')
